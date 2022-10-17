55445feecd0591643c80be5f878c05e4f3c16081
Decho Kocharin <sourcedecho@outlook.com>
2022-10-17 12:28:47 +0700

    Update 2022-10-14-shares_solo.sql

--- sql/2022-10-14-shares_solo.sql
+++ sql/2022-10-14-shares_solo.sql
@@ -1,6 +1,7 @@
 -- Recent additions to add after db init (.gz)
 -- mysql yaamp -p < file.sql
 
- -- add blocks for solo function
+ -- add shares for solo function
 
-ALTER TABLE `blocks` ADD `solo` TINYINT(1) NULL DEFAULT NULL AFTER `category`;
+ALTER TABLE `shares` ADD `solo` TINYINT(1) NULL DEFAULT NULL AFTER `algo`;
+
