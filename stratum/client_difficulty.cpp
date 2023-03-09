6a122be937f09c7eb55836da7178482f5885e956
Decho Kocharin <sourcedecho@outlook.com>
2023-03-09 11:40:27 +0700

    Update client_difficulty.cpp
    thank from TheOneWhoWas

--- stratum/client_difficulty.cpp
+++ stratum/client_difficulty.cpp
@@ -1,102 +1,134 @@
-
 #include "stratum.h"
 
 double client_normalize_difficulty(double difficulty)
-{
-	if(difficulty < g_stratum_min_diff) difficulty = g_stratum_min_diff;
-	else if(difficulty < 1) difficulty = floor(difficulty*1000/2)/1000*2;
-	else if(difficulty > 1) difficulty = floor(difficulty/2)*2;
-	if(difficulty > g_stratum_max_diff) difficulty = g_stratum_max_diff;
-	return difficulty;
+{           // https://github.com/MicroBitcoinOrg/Yiimp
+	if(difficulty < g_stratum_min_diff)
+        difficulty = g_stratum_min_diff;
+	else if(difficulty < 1) 
+        difficulty = floor(difficulty*100000000/2)/100000000*2;
+	else if(difficulty > 1) 
+        difficulty = floor(difficulty/2)*2;
+	if(difficulty > g_stratum_max_diff) 
+        difficulty = g_stratum_max_diff;
+
+            /* output normalized difficulty value */
+    //debuglog("Difficulty amount altered by: %f\n", difficulty);
+    debuglog("Difficulty amount altered by: %f - %f - %f - %f\n", difficulty, g_stratum_min_diff, g_stratum_max_diff, g_stratum_difficulty);
+    return difficulty;
 }
 
 void client_record_difficulty(YAAMP_CLIENT *client)
 {
-	if(client->difficulty_remote)
-	{
-		client->last_submit_time = current_timestamp();
-		return;
-	}
+    if (client->difficulty_remote) {
+        client->last_submit_time = current_timestamp();
+        return;
+    }
 
-	int e = current_timestamp() - client->last_submit_time;
-	if(e < 500) e = 500;
-	int p = 5;
+    const int MIN_SUBMIT_INTERVAL_MS = 500;
+    const int SMOOTHING_FACTOR = 95;
+    const int SUBMISSIONS_PER_MINUTE = 60 * 1000 / MIN_SUBMIT_INTERVAL_MS;
 
-	client->shares_per_minute = (client->shares_per_minute * (100 - p) + 60*1000*p/e) / 100;
-	client->last_submit_time = current_timestamp();
+    int time_since_last_submit = current_timestamp() - client->last_submit_time;
+    if (time_since_last_submit < MIN_SUBMIT_INTERVAL_MS) {
+        time_since_last_submit = MIN_SUBMIT_INTERVAL_MS;
+    }
 
-//	debuglog("client->shares_per_minute %f\n", client->shares_per_minute);
+    int submissions_per_second = SUBMISSIONS_PER_MINUTE / time_since_last_submit;
+    int new_shares_per_minute = (client->shares_per_minute * SMOOTHING_FACTOR
+                                 + 60 * submissions_per_second) / 100;
+
+    client->shares_per_minute = new_shares_per_minute;
+    client->last_submit_time = current_timestamp();
 }
+
+
 
 void client_change_difficulty(YAAMP_CLIENT *client, double difficulty)
 {
-	if(difficulty <= 0) return;
+    if (difficulty <= 0)
+        return;
 
-	difficulty = client_normalize_difficulty(difficulty);
-	if(difficulty <= 0) return;
+    difficulty = client_normalize_difficulty(difficulty);
+    if (difficulty <= 0)
+        return;
 
-//	debuglog("change diff to %f %f\n", difficulty, client->difficulty_actual);
-	if(difficulty == client->difficulty_actual) return;
+    debuglog("Changing difficulty to %f from %f\n", difficulty, client->difficulty_actual);
 
-	client->difficulty_actual = difficulty;
-	client_send_difficulty(client, difficulty);
+    if (difficulty == client->difficulty_actual)
+        return;
+
+    client->difficulty_actual = difficulty;
+    client_send_difficulty(client, difficulty);
 }
 
 void client_adjust_difficulty(YAAMP_CLIENT *client)
 {
-	if(client->difficulty_remote) {
-		client_change_difficulty(client, client->difficulty_remote);
-		return;
-	}
+    if (client->difficulty_remote) {
+        client_change_difficulty(client, client->difficulty_remote);
+        return;
+    }
 
-	if(client->shares_per_minute > 100)
-		client_change_difficulty(client, client->difficulty_actual*4);
+    double factor = 1.0;
 
-	else if(client->difficulty_fixed)
-		return;
+    if (client->shares_per_minute > 100) {
+        client_change_difficulty(client, client->difficulty_actual * 4);
+    }
+    else if (client->difficulty_fixed) {
+        // do nothing
+    }
+    else if (client->shares_per_minute > 75) {
+        client_change_difficulty(client, client->difficulty_actual * 3.5);
+    }
+    else if (client->shares_per_minute > 50) {
+        client_change_difficulty(client, client->difficulty_actual * 3);
+    }
+    else if (client->shares_per_minute > 25) {
+        client_change_difficulty(client, client->difficulty_actual * 2);
+    }
+    else if (client->shares_per_minute > 20) {
+        client_change_difficulty(client, client->difficulty_actual * 1.5);
+    }
+    else if (client->shares_per_minute < 8) {
+        client_change_difficulty(client, client->difficulty_actual / 2);
+    }
 
-	else if(client->shares_per_minute > 75)
-		client_change_difficulty(client, client->difficulty_actual*3.5);
+    double new_difficulty = client->difficulty_actual * factor;
 
-	else if(client->shares_per_minute > 50)
-		client_change_difficulty(client, client->difficulty_actual*3);
-
-	else if(client->shares_per_minute > 25)
-		client_change_difficulty(client, client->difficulty_actual*2);
-
-	else if(client->shares_per_minute > 20)
-		client_change_difficulty(client, client->difficulty_actual*1.5);
-
-	else if(client->shares_per_minute <  5)
-		client_change_difficulty(client, client->difficulty_actual/2);
+    if (new_difficulty >= g_stratum_difficulty && new_difficulty != client->difficulty_actual) {
+        debuglog("Adjusting difficulty to %f from %f\n", new_difficulty, client->difficulty_actual);
+        client_change_difficulty(client, new_difficulty);
+    }
 }
 
 int client_send_difficulty(YAAMP_CLIENT *client, double difficulty)
 {
-//	debuglog("%s diff %f\n", client->sock->ip, difficulty);
-	client->shares_per_minute = YAAMP_SHAREPERSEC;
+    client->shares_per_minute = YAAMP_SHAREPERSEC;
 
-	if(difficulty >= 1)
-		client_call(client, "mining.set_difficulty", "[%.0f]", difficulty);
-	else
-		client_call(client, "mining.set_difficulty", "[%0.8f]", difficulty);
-	return 0;
+    char buf[64];
+    if (difficulty >= 1.0) {
+        snprintf(buf, sizeof(buf), "[%.0f]", difficulty);
+    } else {
+        snprintf(buf, sizeof(buf), "[%0.8f]", difficulty);
+    }
+
+    client_call(client, "mining.set_difficulty", buf);
+
+    return 0;
 }
 
 void client_initialize_difficulty(YAAMP_CLIENT *client)
 {
-	char *p = strstr(client->password, "d=");
-	char *p2 = strstr(client->password, "decred=");
-	if(!p || p2) return;
+    char *p = strstr(client->password, "d=");
+    char *p2 = strstr(client->password, "decred=");
+    if (!p || p2)
+        return;
 
-	double diff = client_normalize_difficulty(atof(p+2));
-	uint64_t user_target = diff_to_target(diff);
+    double diff = client_normalize_difficulty(atof(p + 2));
+    uint64_t user_target = diff_to_target(diff);
 
-//	debuglog("%016llx target\n", user_target);
-	if(user_target >= YAAMP_MINDIFF && user_target <= YAAMP_MAXDIFF)
-	{
-		client->difficulty_actual = diff;
-		client->difficulty_fixed = true;
-	}
-
+    //	debuglog("%016llx target\n", user_target);
+    if (user_target >= g_stratum_min_diff && user_target <= g_stratum_max_diff) {
+        client->difficulty_actual = diff;
+        client->difficulty_fixed = false;
+    }
 }
